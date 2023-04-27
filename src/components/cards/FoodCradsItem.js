2b323d465a5b3f39cad45e986923cee60ffba156
kingdev <kingdev5809@gmail.com>
2023-04-27 16:48:46 +0500

    aa

--- src/components/cards/FoodCradsItem.js
+++ src/components/cards/FoodCradsItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function FoodCradsItem({ data }) {
+  const randomPrice = Math.floor(Math.random() * (15 - 2 + 2)) + 2;
   return (
     <div>
       <div className="card max-w-xs p-3 m-auto  rounded-xl text-center items-center mt-16 cursor-pointer  relative bg-black-200 ">
@@ -9,7 +10,7 @@ function FoodCradsItem({ data }) {
         </div>
         <div className="pt-[140px] w-[55%] m-auto  text-[14px]  ">
           <h2 className="text-gray-200 font-[500]">{data.strMeal}</h2>
-          <h3 className="text-gray-300 py-3 ">$2.29</h3>
+          <h3 className="text-gray-300 py-3 ">${randomPrice}</h3>
           <p className="text-gray-400  ">20 bowls avilable</p>
         </div>
       </div>
